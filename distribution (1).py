34de66506329b1564bb80c2e0376bd214225fe8e
SapryginIgor <93043160+SapryginIgor@users.noreply.github.com>
2024-02-27 12:24:30 +0300

    Add files via upload

---
+++ distribution (1).py
@@ -0,0 +1,51 @@
+import numpy as np
+
+class LaplaceDistribution:    
+    @staticmethod
+    def mean_abs_deviation_from_median(x: np.ndarray):
+        '''
+        Args:
+        - x: A numpy array of shape (n_objects, n_features) containing the data
+          consisting of num_train samples each of dimension D.
+        '''
+        ####
+        # Do not change the class outside of this block
+        # Your code here
+        median_per_feature = np.median(x, axis=0)
+        diff = np.absolute(x - median_per_feature)
+        diff = np.mean(diff, axis=0)
+        return diff
+        ####
+
+    def __init__(self, features):
+        '''
+        Args:
+            feature: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        ####
+        # Do not change the class outside of this block
+        self.loc = np.median(features, axis=0)
+        self.scale = self.mean_abs_deviation_from_median(features)
+        ####
+
+
+    def logpdf(self, values):
+        '''
+        Returns logarithm of probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        ####
+        # Do not change the class outside of this block
+        diff = np.absolute(values - self.loc) / (-self.scale)
+        return diff - np.log(2*self.scale)
+        ####
+        
+    
+    def pdf(self, values):
+        '''
+        Returns probability density at every input value.
+        Args:
+            values: A numpy array of shape (n_objects, n_features). Every column represents all available values for the selected feature.
+        '''
+        return np.exp(self.logpdf(values))
